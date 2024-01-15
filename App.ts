033084dfcb95008975f940ea3d7d1662a4495925
Nigel Leck <monster.flying.s@gmail.com>
2024-01-15 16:11:24 +1100

    Update App.ts

--- App.ts
+++ App.ts
@@ -1,5 +1,4 @@
 import { parseArgs } from "https://deno.land/std@0.212.0/cli/parse_args.ts";
-import { addTag } from "./src/TagsInterface.ts";
 import { TagAndRelease } from "./src/TagAndRelease.ts";
 
 const args = parseArgs(Deno.args);
